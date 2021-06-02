6bbf160fdd6529ea9311171660486ac08a184350
Nick McMurdie <nickmcmurdie@gmail.com>
2021-06-01 20:25:23 -0700

    Add image to base64 endpoint

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+'use strict'
 const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', () => {
+    throw new Error('Invalid endpoint');
+});
 
-app.get('/about', (req, res) => res.send('About Page Route'));
-
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/basic', (req, res) => {
+    let url = req.query.url;
+    fetch(url)
+        .then(async response => {
+            let content = await response.buffer();
+            let body = `data:${response.headers.get('content-type')};base64,${content.toString('base64')}`;
+            res.send(body);
+        })
+        .catch(err => {
+            throw new Error("Unable to retrieve image");
+        });
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port);
