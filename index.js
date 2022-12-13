f2893e7e68e1332a09947bf6ecf935214984a02a
Nick McMurdie <nickmcmurdie@gmail.com>
2022-12-12 19:45:06 -0700

    Experimental thumbnail generator

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const sharp = require('sharp');
+const axios = require("axios");
+const cheerio = require("cheerio");
 require('dotenv').config();
+
+const imgTypes = ["jpg", "png", "jpeg", "webp"];
+const IMAGE_SIZE_THRESHOLD = 800;
 
 const app = express();
 app.use(cors({
@@ -14,17 +19,77 @@ app.get('/', (req, res) => {
     res.status(400).send("Error: Invalid Endpoint");
 });
 
-app.get('/fetchContent', async (req, res) => {
-    res.set('Cache-control', 'public, max-age=86400');
-    let url = req.query.url;
+const getImageURL = (str) => {
+    if (!str) return undefined;
 
-    if (url) {
-        const response = await fetch(url);
-        const body = await response.text();
-        res.send(body);
+    try {
+        const url = new URL(str);
+        const filetype = url.pathname.split('.').pop().trim();
+        if (imgTypes.includes(filetype)) return `${url.protocol}//${url.host}${url.pathname}`;
+    }
+    catch (exception) {
+        return undefined;
+    }
+}
+
+const resizeImage = (res, url, w) => {
+    fetch(url)
+        .then(async response => {
+            let buffer = await response.buffer();
+            sharp(buffer).resize(+w).toBuffer()
+                .then(data => {
+                    res.contentType(response.headers.get('content-type'));
+                    res.send(data);
+                })
+                .catch(err => {
+                    res.status(404).send("Error: Image Not Found");
+                })
+        })
+        .catch(err => {
+            res.status(404).send("Error: Unable to Fetch Image");
+        });
+}
+
+app.get('/thumbnail', async (req, res) => {
+    res.set('Cache-control', 'public, max-age=86400');
+    let { url, w } = req.query;
+
+    if (url && w) {
+        try {
+            const { data } = await axios.get(url, { 
+                headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+            });
+            const $ = cheerio.load(data);
+            const sources = $('source');
+            const images = $('img');
+
+            // Find largest image
+            let maxImage;
+            let maxImageSize = -2;
+
+            let processImg = (el, src_attr, size_attr) => {
+                const source = $(el);
+                const src = source.attr(src_attr);
+                const size = source.attr(size_attr);
+                const sizeInt = size ? parseInt(size) : -1;
+                
+                const imageURL = getImageURL(src);
+                if (sizeInt > maxImageSize && imageURL) maxImage = imageURL;
+                if (maxImageSize >= IMAGE_SIZE_THRESHOLD) return false;
+            };
+
+            sources.each((_idx, el) => processImg(el, 'data-srcset', 'sizes'));
+            images.each((_idx, el) => processImg(el, 'src', 'width'));
+
+            resizeImage(res, maxImage, w);
+        } catch(err) {
+            console.log(err);
+            res.status(404).send("Error: Unable to Fetch Images from Page");
+        }
+        
     }
     else {
-        res.status(400).send("Error: Missing URL Parameter");
+        res.status(400).send("Error: Missing URL or Width Parameter");
     }
 });
 
@@ -50,21 +115,7 @@ app.get('/resize', (req, res) => {
     res.set('Cache-Control', 'public, max-age=86400');
     let { url, w } = req.query;
 
-    if (url && w) fetch(url)
-        .then(async response => {
-            let buffer = await response.buffer();
-            sharp(buffer).resize(+w).toBuffer()
-                .then(data => {
-                    res.contentType(response.headers.get('content-type'));
-                    res.send(data);
-                })
-                .catch(err => {
-                    res.status(404).send("Error: Image Not Found");
-                })
-        })
-        .catch(err => {
-            res.status(404).send("Error: Unable to Fetch Image");
-        });
+    if (url && w) resizeImage(res, url, w);
     else {
         res.status(400).send("Error: Missing URL or Width Parameter");
     }
